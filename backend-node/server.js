9d11ecae8903637197c987c1d9ab255799652d3f
zphtet <zinphtet.215108@gmail.com>
2023-09-17 20:38:53 +0630

    express install and command

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,1 +1,12 @@
 // server code will goes here.
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! HI");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
